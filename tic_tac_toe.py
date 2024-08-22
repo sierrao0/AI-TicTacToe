76c2fcb174713bd06b6dfc9d801ca6db32f08111
sierrao0 <kamisops@gmail.com>
2024-08-21 20:37:17 -0500

    Implemented Min-Max functions, changed some texts and formatted code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,97 +1,111 @@
 import random
 import os
-clear = lambda: os.system('clear')
 
-# Letter choice function
-def select_letter():
-    let=""
-    auto_let=""
-    # ask user to select a letter (X or O)
-    while(let != "x" and let != "o"):
-        let=input("Select X or O: ").replace(" ","").strip().lower()
-        if let == "x":
-            auto_let="o"
-        else:
-            auto_let="x"
-    return let, auto_let
+if os.name == 'posix':
+    clear = lambda: os.system('clear')
+else:
+    clear = lambda: os.system('cls')
 
 # Un estado en Triqui se puede representar mediante una matriz de 3x3 
 # que contiene 'X', 'O' o un espacio vacío. Cada disposición diferente 
-# de símbolos en el tablero es un estado único.
+# de símbolos en el tablero es un ESTADO UNICO.
 
-# Board cleaning/creating function
-def clean_board():
+# Funcion creadora/limpiadora del tablero.
+def nuevo_tablero():
     #  An empty board for X and O values
     brd=[[' ', ' ', ' '],
          [' ', ' ', ' '],
          [' ', ' ', ' ']]
     return brd
 
-# Board checking function
-def is_board_full(board):
-    count = 0
+# Funcion heuristica para valorar un estado
+def heuristica(board):
+    # Comprueba filas, columnas y diagonales en caso de una victoria y evalua.
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] != ' ':
+            return 10 if row[0] == 'X' else -10
+
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != ' ':
+            return 10 if board[0][col] == 'X' else -10
+
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ':
+        return 10 if board[0][0] == 'X' else -10
+
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != ' ':
+        return 10 if board[0][2] == 'X' else -10
+
+    return 0
+
+# Funcion del algoritmo Min-Max
+def minmax(board, nivel, is_max):
+    puntaje = heuristica(board)
+
+    # Casos Base
+    if puntaje == 10:
+        return puntaje - nivel
+    if puntaje == -10:
+        return puntaje + nivel
+    if is_board_full(board):
+        return 0
+
+    if is_max:
+        best = -1000
+
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    board[i][j] = 'X'
+                    best = max(best, minmax(board, nivel + 1, not is_max))
+                    board[i][j] = ' '
+        return best
+    
+    else:
+        best = 1000
+
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    board[i][j] = 'O'
+                    best = min(best, minmax(board, nivel + 1, not is_max))
+                    board[i][j] = ' '
+        return best
+    
+# Movimiento de la "IA"
+def jugada_IA(board, letter):
+    best_val = -1000
+    best_move = (-1, -1)
+
     for i in range(3):
         for j in range(3):
-            if board[i][j] != ' ':
-                count += 1
-    return count
+            if board[i][j] == ' ':
+                board[i][j] = letter
+                move_val = minmax(board, 0, False)
+                board[i][j] = ' '
+
+                if move_val > best_val:
+                    best_move = (i, j)
+                    best_val = move_val
+                    
+    board[best_move[0]][best_move[1]] = letter
+    
+#--------------------------------------------------------
+#|<FUNCIONES MODIFICADAS DE LA IMPLEMENTACION DE NUHAGH>|
+#--------------------------------------------------------
+
+# Board checking function
+def is_board_full(board):
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == ' ':
+                return False
+    return True
 
 # To insert a letter (X or O) in a specific position
 def insert_letter(board,letter,pos):
     i = int((pos-1)/3)
     j = ((pos-1) % 3)
     board[i][j] = letter
-
-# To take computer moves
-def computer_move(board,letter):
-    computer_letter=letter
-    possible_moves=[]
-    available_corners=[]
-    available_edges=[]
-    available_center=[]
-    position=-1
-
-    # All possible moves
-    for i in range(1,len(board)):
-        if board[i] ==" ":
-            possible_moves.append(i)
-
-    # If the position can make X or O wins!
-    # The computer will choose it to win or ruin a winning of the user
-    for let in ["x","o"]:
-        for i in possible_moves:
-            board_copy=board[:]
-            board_copy[i] = let
-            if is_winner(board_copy,let):
-                position=i
-
-
-    # If computer cannot win or ruin a winning, then it will choose a random position starting
-    # With the corners, the center then the edges
-    if position == -1:
-        for i in range(len(board)):
-            # An empty index on the board
-            if board[i]==" ":
-                if i in [1,3,7,9]:
-                    available_corners.append(i)
-                if i == 5:
-                    available_center.append(i)
-                if i in [2,4,6,8]:
-                    available_edges.append(i)
-        # Check corners first
-        if len(available_corners)>0:
-            # select a random position in the corners
-            position=random.choice(available_corners)
-        # then check the availability of the center
-        elif len(available_center)>0:
-            # select the center as the position
-            position=available_center[0]
-        # lastly, check the availability of the edges
-        elif len(available_edges)>0:
-            # select a random position in the edges
-            position=random.choice(available_edges)
-    # fill the position with the letter
-    board[position]=computer_letter
 
 # to draw the board
 def draw_board(board):
@@ -106,9 +120,8 @@ def draw_board(board):
     print(" "+board[2][0]+" | "+board[2][1]+" | "+board[2][2]+" ") 
     print("\n")
 
-# to check if a specific "player" is the winner
+# Comprueba si el jugador humano es ganador
 def is_winner(board, letter):
-    # Check rows, columns, and diagonals for a win
     for row in board:
         if row[0] == row[1] == row[2] and row[0] == letter:
             return True
@@ -128,61 +141,67 @@ def is_winner(board, letter):
 # to repeat the game
 def repeat_game():
 
-    repeat=input("Play again? Press y for yes and n for no: ")
+    repeat=input("\nDesea jugar de nuevo? (y/n) ")
     while repeat != "n" and repeat != "y":
-        repeat=input("PLEASE, press y for yes and n for no: ")
+        repeat=input("Por favor, presione y para si y n para no: ")
     return repeat
 
 # to play the game
 def play_game():
-
-    letter, auto_letter= select_letter()
+    letter = 'O'
+    auto_letter = 'X'
     # clean the board
-    board=clean_board()
+    board=nuevo_tablero()
     draw_board(board)
     # check if there are empty positions on the board
     while is_board_full(board) == False:
         try:
-            position=int(input("Select a position (1-9) to place an "+letter+" : " ))
+            position=int(input("Seleccione la posicion (1-9) donde quiere marcar: " ))
 
         except:
-            position=int(input("PLEASE enter position using only NUMBERS from 1-9: "))
+            position=int(input("Por favor, solo numeros del 1 al 9: "))
 
         # check if user selects out of range position
         while (position not in range(1,10)):
-            position=int(input("Please, choose another position to place an "+letter+" from 1 to 9 :"))
+            position=int(input("Por favor, elija una posicion valida del 1 al 9: "))
 
         # check if user selects an occupied position by X or O
         pos_i = int((position-1)/3)
         pos_j = ((position-1) % 3)
         while (board[pos_i][pos_j] != " "):
-            position=int(input("Please, choose an EMPTY position to place an "+letter+" from 1 to 9: "))
+            position=int(input("Por favor, elija una posicion VACIA del 1 al 9: "))
 
         # put the letter in the selected position & computer plays then draw the board
         insert_letter(board,letter,position)
         
         # computer move
-        computer_move(board,auto_letter)
+        jugada_IA(board,auto_letter)
         
         # draw the board
         draw_board(board)
 
-        if is_winner(board,letter):
-            print("Congratulations! You Won.")
+        if is_winner(board, letter):
+            print("***************************")
+            print("Felicitaciones, has ganado!")
+            print("***************************")
             return repeat_game()
-        elif is_winner(board,auto_letter):
-            print("Hard Luck! Computer won")
+        elif is_winner(board, auto_letter):
+            print("**************************************")
+            print("Que mala suerte, la IA te ha superado!")
+            print("**************************************")
             return repeat_game()
 
     # if " " not in board:
     if is_board_full(board):
-        print("Tie Game :)")
+        print("*"*69)
+        print("Que despliegue de calidad. Sin embargo, esta vez no habra un ganador!")
+        print("*"*69)
         return repeat_game()
 
 # Start the game
 if __name__ == "__main__":
     clear()
-    print("Welcome to T-IA-c Toe. \n")
+    print("Bienvenido a T-IA-c Toe.")
     repeat="y"
     while(repeat=="y"):
         repeat=play_game()
